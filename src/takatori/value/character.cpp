a876b4124556a55883fb892b9c8e548ac3c9c12a
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2024-08-27 19:33:31 +0900

    feat: introduce `takatori::value::truncate_utf8`.

--- src/takatori/value/character.cpp
+++ src/takatori/value/character.cpp
@@ -50,4 +50,75 @@ std::ostream& operator<<(std::ostream& out, character const& value) {
     return out << "character(" << value.get() << ")";
 }
 
+std::string_view truncate_utf8(std::string_view str, std::size_t size) {
+    if (str.size() <= size) {
+        return str;
+    }
+
+    constexpr unsigned char mask1 = 0b1000'0000;
+    constexpr unsigned char mask2 = 0b1100'0000;
+    constexpr unsigned char mask3 = 0b1110'0000;
+    constexpr unsigned char mask4 = 0b1111'0000;
+    constexpr unsigned char mask5 = 0b1111'1000;
+
+    constexpr unsigned char body2 = 0b1000'0000;
+    constexpr unsigned char head1_1 = 0b0000'0000;
+    constexpr unsigned char head3_2 = 0b1100'0000;
+    constexpr unsigned char head4_3 = 0b1110'0000;
+    constexpr unsigned char head5_4 = 0b1111'0000;
+
+    constexpr std::size_t max_sequence_size = 4;
+
+    // first, we find for the head of the last UTF-8 sequence
+    // we don't need to search for the 4-byte UTF-8 sequence.
+    for (std::size_t backward_offset = 1; backward_offset <= max_sequence_size - 1; ++backward_offset) {
+        if (backward_offset > size) {
+            // no head of UTF-8 sequences found
+            break;
+        }
+        auto position = size - backward_offset;
+        auto byte = static_cast<unsigned char>(str[position]);
+
+        if ((byte & mask2) == body2) {
+            // found non-head UTF-8 element, continue to backward.
+            continue;
+        }
+
+        if ((byte & mask1) == head1_1) {
+            // found 1-byte UTF-8 sequence
+            return str.substr(0, position + 1);
+        }
+        if ((byte & mask3) == head3_2) {
+            // found 2-byte UTF-8 sequence
+            if (backward_offset >= 2) {
+                return str.substr(0, position + 2);
+            }
+            // truncate the current 2-byte UTF-8 sequence
+            return str.substr(0, position);
+        }
+        if ((byte & mask4) == head4_3) {
+            // found 3-byte UTF-8 sequence
+            if (backward_offset >= 3) {
+                return str.substr(0, position + 3);
+            }
+            // truncate the current 3-byte UTF-8 sequence
+            return str.substr(0, position);
+        }
+        if ((byte & mask5) == head5_4) {
+            // found 4-byte UTF-8 sequence
+            // truncate the current 4-byte UTF-8 sequence
+            return str.substr(0, position);
+        }
+        // We not found a head of UTF-8 sequence.
+        // It because one of the following reasons:
+        // 1. the input string is a well-formed UTF-8 string, and the last one is a 4-byte UTF-8 sequence.
+        // 2. the input string is not a well-formed UTF-8 string.
+        // Anyway, we can obtain the expected result by trim to the originally specified size.
+        break;
+    }
+
+    // if the head of the last UTF-8 sequence is not found, we just return the truncated string
+    return str.substr(0, size);
+}
+
 } // namespace takatori::value
