e0c713839710de9dc3a3d4e3b11041e373f76764
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2026-02-13 13:57:36 +0900

    feat: introduce `join_kind::left_outer_at_most_one`.

--- include/takatori/relation/join_kind.h
+++ include/takatori/relation/join_kind.h
@@ -5,8 +5,6 @@
 #include <string_view>
 #include <utility>
 
-#include <cstdlib>
-
 #include <takatori/util/enum_set.h>
 #include <takatori/util/enum_tag.h>
 
@@ -22,6 +20,8 @@ enum class join_kind {
     left_outer,
     /// @brief full outer join.
     full_outer,
+    /// @brief left outer join, at most one row per left row.
+    left_outer_at_most_one,
     /// @brief left semi join
     semi,
     /// @brief left anti join
@@ -39,13 +39,14 @@ using join_kind_set = util::enum_set<
  * @param value the target value
  * @return the corresponded string representation
  */
-constexpr inline std::string_view to_string_view(join_kind value) noexcept {
+constexpr std::string_view to_string_view(join_kind value) noexcept {
     using namespace std::string_view_literals;
     using kind = join_kind;
     switch (value) {
         case kind::inner: return "inner"sv;
         case kind::left_outer: return "left_outer"sv;
         case kind::full_outer: return "full_outer"sv;
+        case kind::left_outer_at_most_one: return "left_outer_at_most_one"sv;
         case kind::semi: return "semi"sv;
         case kind::anti: return "anti"sv;
     }
@@ -90,12 +91,13 @@ inline constexpr join_kind_tag_t<Kind> join_kind_tag {};
  * @return the callback result
  */
 template<class Callback, class... Args>
-inline auto dispatch(Callback&& callback, join_kind tag_value, Args&&... args) {
+auto dispatch(Callback&& callback, join_kind tag_value, Args&&... args) {
     using kind = join_kind;
     switch (tag_value) {
         case kind::inner: return util::enum_tag_callback<kind::inner>(std::forward<Callback>(callback), std::forward<Args>(args)...);
         case kind::left_outer: return util::enum_tag_callback<kind::left_outer>(std::forward<Callback>(callback), std::forward<Args>(args)...);
         case kind::full_outer: return util::enum_tag_callback<kind::full_outer>(std::forward<Callback>(callback), std::forward<Args>(args)...);
+        case kind::left_outer_at_most_one: return util::enum_tag_callback<kind::left_outer_at_most_one>(std::forward<Callback>(callback), std::forward<Args>(args)...);
         case kind::semi: return util::enum_tag_callback<kind::semi>(std::forward<Callback>(callback), std::forward<Args>(args)...);
         case kind::anti: return util::enum_tag_callback<kind::anti>(std::forward<Callback>(callback), std::forward<Args>(args)...);
     }
